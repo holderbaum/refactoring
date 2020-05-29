476bb732d881888737e104f3248de506218c5161
Jakob Holderbaum <jakob@holderbaum.io>
2020-05-29 07:40:35 +0200

    Update package

--- src/main/java/io/holderbaum/App.java
+++ src/main/java/io/holderbaum/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package io.holderbaum;
 
 /**
  * Hello world!
--- src/test/java/io/holderbaum/AppTest.java
+++ src/test/java/io/holderbaum/AppTest.java
@@ -1,17 +1,12 @@
-package org.example;
+package io.holderbaum;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
